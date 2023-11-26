8fca7b8ce6ce9d51dfcc3fa6a1e6a703df128a3a
Edgar Bjørntvedt <edgar.bjorntvedt@gmail.com>
2023-11-26 23:10:25 +0100

    adding a simple api with lambdas and dynamodb

--- README.md
+++ README.md
@@ -12,3 +12,28 @@ It uses the [.NET CLI](https://docs.microsoft.com/dotnet/articles/core/) to comp
 * `cdk deploy`       deploy this stack to your default AWS account/region
 * `cdk diff`         compare deployed stack with current state
 * `cdk synth`        emits the synthesized CloudFormation template
+
+https://878rhuh3d9.execute-api.eu-west-1.amazonaws.com/prod/items
+
+
+example curl 
+```shell
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' --header 'x-tfso-trackingid: manualExecutioncarina' --header 'x-tfso-license: 00000000-0000-0000-0000-000000000001;611907646440257;1' 'http://bank-ledger-aggregator.api2.24sevenoffice.com/incomingpayments/matching/3445d593-8bcf-489f-b065-6f9420b65495?autoPost=false&limit=3000' 
+
+```
+
+curl for getting items from aws
+
+```shell 
+curl -X GET --header 'Accept: application/json' 'https://878rhuh3d9.execute-api.eu-west-1.amazonaws.com/prod/items'
+```
+
+curl for posting items to aws
+
+```shell 
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ \ 
+   "ItemId": "1", \ 
+   "AnExampleField": "stuff", \ 
+ }' 'https://878rhuh3d9.execute-api.eu-west-1.amazonaws.com/prod/items'
+```
+
--- src/AwsDotnetCdk/AwsDotnetCdkStack.cs
+++ src/AwsDotnetCdk/AwsDotnetCdkStack.cs
@@ -1,5 +1,12 @@
+using System.Collections.Generic;
 using Amazon.CDK;
+using Amazon.CDK.AWS.S3;
+using Amazon.CDK.AWS.APIGateway;
+using Amazon.CDK.AWS.DynamoDB;
+using Amazon.CDK.AWS.Lambda;
 using Constructs;
+using IResource = Amazon.CDK.AWS.APIGateway.IResource;
+
 
 namespace AwsDotnetCdk
 {
@@ -8,6 +15,226 @@ namespace AwsDotnetCdk
         internal AwsDotnetCdkStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
         {
             // The code that defines your stack goes here
+
+            // S3 Bucket
+            new Bucket(this, "MyFirstBucket", new BucketProps
+            {
+                Versioned = true,
+                RemovalPolicy = RemovalPolicy.DESTROY,
+                AutoDeleteObjects = true
+            });
+
+            // DynamoDB Table
+            var dynamoTable = new Table(
+                this,
+                "items",
+                new TableProps()
+                {
+                    PartitionKey = new Attribute()
+                    {
+                        Name = "ItemId",
+                        Type = AttributeType.STRING
+                    },
+                    TableName = "Items",
+
+                    /*
+                    *  The default removal policy is RETAIN, which means that cdk destroy will not attempt to delete
+                    * the new table, and it will remain in your account until manually deleted. By setting the policy to
+                    * DESTROY, cdk destroy will delete the table (even if it has data in it)
+                    */
+                    RemovalPolicy = RemovalPolicy.DESTROY // NOT recommended for production code
+                });
+
+
+            // Lambda Functions
+            var createOneLambda = new DockerImageFunction(
+                this,
+                "createItemFunction",
+                new DockerImageFunctionProps()
+                {
+                    Code = BuildDockerImageCode("Create"),
+                    FunctionName = "createItemFunction",
+                    Timeout = Duration.Seconds(10)
+                }
+            );
+
+            var getOneLambda = new DockerImageFunction(
+                this,
+                "getOneItemFunction",
+                new DockerImageFunctionProps()
+                {
+                    Code = BuildDockerImageCode("GetOne"),
+                    FunctionName = "getOneItemFunction",
+                    Timeout = Duration.Seconds(10)
+                }
+            );
+
+            var getAllLambda = new DockerImageFunction(
+                this,
+                "getAllItemsFunction",
+                new DockerImageFunctionProps()
+                {
+                    Code = BuildDockerImageCode("GetAll"),
+                    FunctionName = "getAllItemsFunction",
+                    Timeout = Duration.Seconds(10)
+                }
+            );
+
+            var getAllLambdaSimple = new Function(
+                this, "getAllItemsSimpleFunction", new FunctionProps
+                {
+                    Runtime = Runtime.DOTNET_6,
+                    Handler = "GetAllSimple::GetAllSimple.Function::FunctionHandler",
+                    Code = Code.FromAsset("./dist/GetAllSimple"),
+                    FunctionName = "getAllItemsSimpleFunction",
+                    Timeout = Duration.Seconds(10)
+                });
+
+            var updateOneLambda = new DockerImageFunction(
+                this,
+                "updateItemFunction",
+                new DockerImageFunctionProps()
+                {
+                    Code = BuildDockerImageCode("UpdateOne"),
+                    FunctionName = "updateItemFunction",
+                    Timeout = Duration.Seconds(10)
+                }
+            );
+
+            var deleteOneLambda = new DockerImageFunction(
+                this,
+                "deleteItemFunction",
+                new DockerImageFunctionProps()
+                {
+                    Code = BuildDockerImageCode("DeleteOne"),
+                    FunctionName = "deleteItemFunction",
+                    Timeout = Duration.Seconds(10)
+                }
+            );
+
+            // Grant the Lambda function read / write / describe access to the DynamoDB table
+            dynamoTable.GrantReadData(getAllLambda);
+            dynamoTable.GrantDescribeTable(getAllLambda);
+
+            dynamoTable.GrantReadData(getAllLambdaSimple);
+            dynamoTable.GrantDescribeTable(getAllLambdaSimple);
+
+            dynamoTable.GrantReadData(getOneLambda);
+            dynamoTable.GrantDescribeTable(getOneLambda);
+
+            dynamoTable.GrantReadWriteData(createOneLambda);
+            dynamoTable.GrantDescribeTable(createOneLambda);
+
+            dynamoTable.GrantReadWriteData(updateOneLambda);
+            dynamoTable.GrantDescribeTable(updateOneLambda);
+
+            dynamoTable.GrantReadWriteData(deleteOneLambda);
+            dynamoTable.GrantDescribeTable(deleteOneLambda);
+
+
+            // API Gateway
+            // Integrate the Lambda functions with the API Gateway resource
+            var getAllIntegration = new LambdaIntegration(getAllLambda);
+            var createOneIntegration = new LambdaIntegration(createOneLambda);
+            var getOneIntegration = new LambdaIntegration(getOneLambda);
+            var updateOneIntegration = new LambdaIntegration(updateOneLambda);
+            var deleteOneIntegration = new LambdaIntegration(deleteOneLambda);
+
+            // Create an API Gateway resource for each of the CRUD operations
+            var api = new RestApi(this,
+              "itemsApi",
+              new LambdaRestApiProps()
+              {
+                  RestApiName = "Items Service"
+              });
+
+            var simple = api.Root.AddResource("simple");
+            simple.AddMethod("GET", new LambdaIntegration(getAllLambdaSimple, new LambdaIntegrationOptions{
+                Proxy = true
+            }));
+            AddCorsOptions(simple);
+
+            var items = api.Root.AddResource("items");
+            items.AddMethod("GET", getAllIntegration);
+            items.AddMethod("POST", createOneIntegration);
+            AddCorsOptions(items);
+
+            var singleItem = items.AddResource("{id}");
+            singleItem.AddMethod("GET", getOneIntegration);
+            singleItem.AddMethod("PATCH", updateOneIntegration);
+            singleItem.AddMethod("DELETE", deleteOneIntegration);
+            AddCorsOptions(singleItem);
         }
+
+        private void AddCorsOptions(IResource apiResource)
+        {
+            apiResource.AddMethod("OPTIONS",
+                new MockIntegration(new IntegrationOptions()
+                {
+                    IntegrationResponses = new IIntegrationResponse[]
+                {
+                new IntegrationResponse()
+                {
+                StatusCode = "200",
+                ResponseParameters = new Dictionary<string, string>()
+                {
+                    {
+                    "method.response.header.Access-Control-Allow-Headers",
+                    "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent'"
+                    },
+                    {
+                    "method.response.header.Access-Control-Allow-Origin", "'*'"
+                    },
+                    {
+                    "method.response.header.Access-Control-Allow-Credentials", "'false'"
+                    },
+                    {
+                    "method.response.header.Access-Control-Allow-Methods", "'OPTIONS,GET,PUT,POST,DELETE'"
+                    }
+                }
+                }
+                },
+                    PassthroughBehavior = PassthroughBehavior.NEVER,
+                    RequestTemplates = new Dictionary<string, string>()
+                {
+                {
+                "application/json", "{\"statusCode\": 200}"
+                }
+                },
+                }),
+                new MethodOptions()
+                {
+                    MethodResponses = new IMethodResponse[]
+                {
+                new MethodResponse()
+                {
+                StatusCode = "200",
+                ResponseParameters = new Dictionary<string, bool>()
+                {
+                    {
+                    "method.response.header.Access-Control-Allow-Headers", true
+                    },
+                    {
+                    "method.response.header.Access-Control-Allow-Methods", true
+                    },
+                    {
+                    "method.response.header.Access-Control-Allow-Credentials", true
+                    },
+                    {
+                    "method.response.header.Access-Control-Allow-Origin", true
+                    }
+                }
+                }
+                }
+                });
+        }
+
+        private DockerImageCode BuildDockerImageCode(string dockerImageName) =>
+          DockerImageCode.FromImageAsset(
+            "src/Lambdas",
+            new AssetImageCodeProps()
+            {
+                File = $"{dockerImageName}/Dockerfile"
+            });
     }
 }

ee9d9bcea29987e0e19279975a50bb34a4d4ccef
Edgar Bjørntvedt <edgar.bjorntvedt@gmail.com>
2023-12-03 16:28:36 +0100

    something

---
+++ apprunner/src/Apprunner/ApprunnerStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace Apprunner
+{
+    public class ApprunnerStack : Stack
+    {
+        internal ApprunnerStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
